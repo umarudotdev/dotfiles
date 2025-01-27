6073723cafafd904ee88fbecb14450f123a535e8
Martins <umarudev@pm.me>
2025-01-26 21:24:57 -0300

    Initial commit

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,15 @@
+# Configuration
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias zconfig='$EDITOR ~/.zshrc'
+alias aconfig='$EDITOR ~/.oh-my-zsh/custom/aliases.zsh'
+
+# Shell
+alias cd="z"
+alias ls="eza --icons"
+alias cat="bat"
+alias grep="rg"
+alias less="moar"
+alias cl="clear"
+
+# Misc
+alias c="code -r"
